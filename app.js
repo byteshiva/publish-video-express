18ecfa7ca235dadc4821e5893b482efdb9a6f84e
root <root@appserver1.(none)>
2016-09-20 19:15:24 +0530

    added new files

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/media', express.static(__dirname + '/media'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+        res.send(text);
+  });
+
+  
 });
 
 module.exports = router;
