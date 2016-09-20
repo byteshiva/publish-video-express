16d46e02c06628da007c57e4cd983f37a1bff726
root <root@appserver1.(none)>
2016-09-20 19:21:12 +0530

    updated port

--- simple_http_server.sh
+++ simple_http_server.sh
@@ -4,14 +4,14 @@
 echo "My Test Page" > test.html
 
 # Start server
-DEBUG=* PORT=9999 npm start &> /dev/null &
+DEBUG=* PORT=5080 npm start &> /dev/null &
 pid=$!
 
 # Give server time to start up
 sleep 1
 
 # request page and print to stdout
-wget -O - http://0.0.0.0:9999/test.html 2> /dev/null
+wget -O - http://0.0.0.0:5080/test.html 2> /dev/null
 
 # Stop server
 #kill "${pid}"
